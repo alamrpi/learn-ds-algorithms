f968f64d48616da056748e4f8e624566eca1f0ff
Zillur Rahman <oit.zillu@gmail.com>
2022-09-30 10:52:27 +0600

    First Commit

---
+++ LearnDSAlgorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LearnDSAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Recursion Example Here
+            var recursion = new Recursion();
+            //recursion.TailRecursion(4);
+            //recursion.HeadRecursion(4);
+            recursion.TreeRecursion(3);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LearnDSAlgorithms/Recursion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnDSAlgorithms
+{
+    public class Recursion
+    {
+        public void TailRecursion(int n)
+        {
+            if (n > 0)
+            {
+                int k = n * n;
+                Console.WriteLine(k);
+                TailRecursion(n - 1);
+            }
+        }
+
+        public void HeadRecursion(int n)
+        {
+            if (n > 0)
+            {
+                HeadRecursion(n - 1);
+                int k = n * n;
+                Console.WriteLine(k);
+            }
+        }
+
+        public void TreeRecursion(int n)
+        {
+            if (n > 0)
+            {
+                TreeRecursion(n - 1);
+                int k = n * n;
+                Console.WriteLine(k);
+                TreeRecursion(n - 1);
+            }
+        }
+    }
+}
