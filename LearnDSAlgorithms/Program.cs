157e636a6d5b323273dadce56124884b39996531
Zillur Rahman <oit.zillu@gmail.com>
2022-10-01 10:41:23 +0600

    example sum of n natural number added

--- LearnDSAlgorithms/Program.cs
+++ LearnDSAlgorithms/Program.cs
@@ -1,14 +1,21 @@
-﻿namespace LearnDSAlgorithms
+﻿using LearnDSAlgorithms.RecursionModule;
+
+namespace LearnDSAlgorithms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //Recursion Example Here
-            var recursion = new Recursion();
+            //var recursion = new Recursion();
             //recursion.TailRecursion(4);
             //recursion.HeadRecursion(4);
-            recursion.TreeRecursion(3);
+            //recursion.TreeRecursion(3);
+
+            var exSum = new ExSum();
+            Console.WriteLine(exSum.SumOfNUsingFormula(5));
+            Console.WriteLine(exSum.SumOfNUsingItteration(5));
+            Console.WriteLine(exSum.SumOfNUsingRecursion(5));
 
             Console.ReadKey();
         }
---
+++ LearnDSAlgorithms/RecursionModule/ExSum.cs
@@ -0,0 +1,31 @@
+﻿namespace LearnDSAlgorithms.RecursionModule
+{
+    public class ExSum
+    {
+        public int SumOfNUsingFormula(int n)
+        {
+            return n * (n + 1) / 2;
+        }
+
+        public int SumOfNUsingItteration(int n)
+        {
+            int sum = 0;
+            int i = 1;
+            while (i <= n)
+            {
+                sum += i;
+                i++;
+            }
+
+            return sum;
+        }
+
+        public int SumOfNUsingRecursion(int n)
+        {
+            if (n == 1)
+                return n;
+
+            return SumOfNUsingRecursion(n - 1) + n;
+        }
+    }
+}
