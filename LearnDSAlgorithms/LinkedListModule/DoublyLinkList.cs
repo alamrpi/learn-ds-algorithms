dcfef5531dde8b5be144f75f6145eb6563df4f5a
Md Alam Hossain <alam.rpi@hotmail.com>
2022-10-30 12:47:55 +0600

    Doubly linked list module done

--- LearnDSAlgorithms/LinkedListModule/DoublyLinkList.cs
+++ LearnDSAlgorithms/LinkedListModule/DoublyLinkList.cs
@@ -49,6 +49,12 @@
             size++;
         }
 
+        /// <summary>
+        /// Add item in any porition of the linked list
+        /// Time Complexity - o(n) - Linear
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="position"></param>
         public void AddAnyPosition(int element, int position)
         {
             if (position <= 0 || position >= size)
@@ -71,6 +77,76 @@
             p.next = newest;
             newest.prev = p;
             size++;
+        }
+
+        /// <summary>
+        /// Remove first item in doubly linked list
+        /// Time Complexoty o(1) - Primitive
+        /// </summary>
+        /// <returns></returns>
+        public int RemoveFirst()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is Empty");
+                return -1;
+            }
+
+            var e = head.element;
+
+            head = head.next;
+            head.prev = null;
+            size--;
+
+            if (IsEmpty())
+            {
+                tail = null;
+            }
+            return e;
+        }
+
+        /// <summary>
+        /// Remove Last item in doubly linked list
+        /// Time Complexity - o(1) - Primitive
+        /// </summary>
+        /// <returns></returns>
+        public int RemoveLast()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is Empty");
+                return -1;
+            }
+
+            var e = tail.element;
+
+            tail = tail.prev;
+            tail.next = null;
+            size--;
+
+            return e;
+        }
+
+        public int RemoveAnyPosition(int position)
+        {
+            if (position <= 0 || position >= size)
+            {
+                Console.WriteLine("Invalid Position.");
+                return -1;
+            }
+
+            var point = head;
+            int i = 1;
+            while (i < position - 1)
+            {
+                point = point.next;
+                i++;
+            }
+            int e = point.next.element;
+            point.next = point.next.next;
+            point.next.prev = point;
+            size--;
+            return e;
         }
 
         /// <summary>
